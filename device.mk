9b1d5fc49a46edee8fc32ee47057a7943bf915f4
himanshuksr0007 <83110103+himanshuksr0007@users.noreply.github.com>
2026-07-13 16:04:28 +0000

    everpal: drop power service temporarily

--- device.mk
+++ device.mk
@@ -284,7 +284,6 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     libmtkperf_client_vendor
 
