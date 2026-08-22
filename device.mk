30c4fd35d0c1a34e704433bee8dac02a5662044f
Addster09 <aadityasingh.cs9@gmail.com>
2026-08-22 23:39:19 +0530

    everpal: Add charging control
    Signed-off-by: Addster09 <aadityasingh.cs9@gmail.com>

--- device.mk
+++ device.mk
@@ -175,6 +175,15 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek-recovery \
     charger_res_images_vendor
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+
 # Hotword Enrollement
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
