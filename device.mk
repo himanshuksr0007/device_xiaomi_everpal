a5fd3c3ec511e4da2fd8470d75be41a5b13a0311
himanshuksr0007 <himanshuksr14@gmail.com>
2026-09-22 22:57:06 +0530

    everpal: complete uncompressed EROFS bring-up
    Set BOARD_EROFS_COMPRESSOR to none since kernel lacks CONFIG_EROFS_FS_ZIP and defaults to lz4hc. Enable BOARD_EROFS_SHARE_DUP_BLOCKS for size saving. Convert fstab logical partitions to erofs with ext4 fallback for system GSI. Fix OTA postinstall filesystem type to erofs.

--- device.mk
+++ device.mk
@@ -57,7 +57,7 @@ PRODUCT_PACKAGES_DEBUG += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
+    FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
